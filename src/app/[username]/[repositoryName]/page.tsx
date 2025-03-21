ba22ca7293a81740419fc7407de454ee2310ecca
Doarakko <2wua4nlyi@gmail.com>
2025-03-21 22:46:31 +0900

    show repository list in repository view page

--- src/app/[username]/[repositoryName]/page.tsx
+++ src/app/[username]/[repositoryName]/page.tsx
@@ -1,10 +1,22 @@
 import type { Commit, Repository } from "@/app/domain";
+import RepositoryCardList from "../../components/RepositoryCardList";
 
 export const runtime = "edge";
 
 export default async function Page(props: {
 	params: Promise<{ username: string; repositoryName: string }>;
 }) {
+	const repositortResponse = await fetch(
+		`${process.env.PUBLIC_URL}/api/repositories`,
+	);
+	if (!repositortResponse.ok) {
+		console.error("Failed to fetch repositories");
+		return;
+	}
+	const repositoryJson: { repositories: Repository[] } =
+		await repositortResponse.json();
+	const repositories = repositoryJson.repositories ?? [];
+
 	const params = await props.params;
 	const response = await fetch(
 		`${process.env.PUBLIC_URL}/api/usernames/${params.username}/repositories/${params.repositoryName}/commits`,
@@ -75,6 +87,9 @@ export default async function Page(props: {
 					</div>
 				))}
 			</div>
+			{repositories.length > 0 && (
+				<RepositoryCardList repositories={repositories} />
+			)}
 		</div>
 	);
 }
