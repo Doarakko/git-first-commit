0cd84dad8b99015e3741d00a8d1d0330ddc24f9d
Doarakko <2wua4nlyi@gmail.com>
2025-03-23 13:53:57 +0900

    do not display commit author image and name when unknown

--- src/app/components/CommitCard.tsx
+++ src/app/components/CommitCard.tsx
@@ -9,21 +9,25 @@ const CommitCard: React.FC<CommitCardProps> = ({ commit }) => {
 	return (
 		<div className="border p-4 rounded-lg min-w-[300px] sm:min-w-[400px] md:min-w-[500px] lg:min-w-[600px] bg-white">
 			<div className="flex items-center gap-4 mb-2">
-				<img
-					src={commit.authorImageUrl}
-					alt={commit.authorId}
-					className="w-10 h-10 rounded-full"
-				/>
-				<div>
-					<a
-						href={commit.authorUrl}
-						target="_blank"
-						rel="noopener noreferrer"
-						className="hover:underline"
-					>
-						{commit.authorId}
-					</a>
-				</div>
+				{commit.authorImageUrl && commit.authorId && (
+					<>
+						<img
+							src={commit.authorImageUrl}
+							alt={commit.authorId}
+							className="w-10 h-10 rounded-full"
+						/>
+						<div>
+							<a
+								href={commit.authorUrl}
+								target="_blank"
+								rel="noopener noreferrer"
+								className="hover:underline"
+							>
+								{commit.authorId}
+							</a>
+						</div>
+					</>
+				)}
 			</div>
 			<a
 				href={commit.url}
