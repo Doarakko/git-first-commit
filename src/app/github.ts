be8cb4ef0f09df21eb6f2c1064b1182320323a5e
Doarakko <2wua4nlyi@gmail.com>
2025-03-30 01:26:00 +0900

    biome format

--- src/app/github.ts
+++ src/app/github.ts
@@ -2,72 +2,84 @@ import { Octokit } from "@octokit/core";
 import type { Endpoints } from "@octokit/types";
 
 export class GitHub {
-	private octokit: Octokit;
+  private octokit: Octokit;
 
-	constructor(token: string) {
-		this.octokit = new Octokit({ auth: token });
-	}
+  constructor(token: string) {
+    this.octokit = new Octokit({ auth: token });
+  }
 
-	// https://github.com/khalidbelk/FirstCommitter
-	public async getFirstCommits(
-		username: string,
-		repositoryName: string,
-		defaultBranch: string,
-	): Promise<Endpoints["GET /repos/{owner}/{repo}/commits"]["response"]["data"]> {
-		const totalPage = await this.getTotalPage(username, repositoryName, defaultBranch);
-		const response = await this.octokit.request("GET /repos/{owner}/{repo}/commits", {
-			owner: username,
-			repo: repositoryName,
-			page: totalPage,
-			per_page: 1,
-		});
+  // https://github.com/khalidbelk/FirstCommitter
+  public async getFirstCommits(
+    username: string,
+    repositoryName: string,
+    defaultBranch: string,
+  ): Promise<
+    Endpoints["GET /repos/{owner}/{repo}/commits"]["response"]["data"]
+  > {
+    const totalPage = await this.getTotalPage(
+      username,
+      repositoryName,
+      defaultBranch,
+    );
+    const response = await this.octokit.request(
+      "GET /repos/{owner}/{repo}/commits",
+      {
+        owner: username,
+        repo: repositoryName,
+        page: totalPage,
+        per_page: 1,
+      },
+    );
 
-		return response.data;
-	}
+    return response.data;
+  }
 
-	private async getTotalPage(
-		username: string,
-		repositoryName: string,
-		branch: string,
-	): Promise<number> {
-		const response = await this.octokit.request("GET /repos/{owner}/{repo}/commits", {
-			owner: username,
-			repo: repositoryName,
-			sha: branch,
-			page: 1,
-			per_page: 1,
-		});
+  private async getTotalPage(
+    username: string,
+    repositoryName: string,
+    branch: string,
+  ): Promise<number> {
+    const response = await this.octokit.request(
+      "GET /repos/{owner}/{repo}/commits",
+      {
+        owner: username,
+        repo: repositoryName,
+        sha: branch,
+        page: 1,
+        per_page: 1,
+      },
+    );
 
-		const linkHeader = response.headers.link;
-		if (linkHeader === undefined) {
-			return 1;
-		}
+    const linkHeader = response.headers.link;
+    if (linkHeader === undefined) {
+      return 1;
+    }
 
-		const links = linkHeader.split(", ");
-		for (const link of links) {
-			const [urlPart, relPart] = link.split("; ");
+    const links = linkHeader.split(", ");
+    for (const link of links) {
+      const [urlPart, relPart] = link.split("; ");
 
-			if (relPart?.includes('rel="last"')) {
-				const url = urlPart.replace(/<|>/g, "");
-				const urlParams = new URL(url).searchParams;
-				const page = urlParams.get("page");
+      if (relPart?.includes('rel="last"')) {
+        const url = urlPart.replace(/<|>/g, "");
+        const urlParams = new URL(url).searchParams;
+        const page = urlParams.get("page");
 
-				return page ? Number.parseInt(page, 10) : 0;
-			}
-		}
+        return page ? Number.parseInt(page, 10) : 0;
+      }
+    }
 
-		return 1;
-	}
+    return 1;
+  }
 
-	public async getRepository(
-		username: string,
-		repositoryName: string,
-	): Promise<Endpoints["GET /repos/{owner}/{repo}"]["response"]["data"]> {
-		const response = await this.octokit.request("GET /repos/{owner}/{repo}", {
-			owner: username,
-			repo: repositoryName,
-		});
+  public async getRepository(
+    username: string,
+    repositoryName: string,
+  ): Promise<Endpoints["GET /repos/{owner}/{repo}"]["response"]["data"]> {
+    const response = await this.octokit.request("GET /repos/{owner}/{repo}", {
+      owner: username,
+      repo: repositoryName,
+    });
 
-		return response.data;
-	}
+    return response.data;
+  }
 }
