15184efdd79e07fcdfc33f3b5cbf30fad7047b44
Doarakko <2wua4nlyi@gmail.com>
2025-03-29 20:04:50 +0900

    show suggestions on form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,9 @@ export default function Home() {
 	const [loading, setLoading] = useState(false);
 	const [progress, setProgress] = useState(0);
 	const [error, setError] = useState<string | null>(null);
+	const [suggestions, setSuggestions] = useState<Repository[]>([]);
+	const [inputValue, setInputValue] = useState("");
+	const [selectedIndex, setSelectedIndex] = useState(-1);
 
 	useEffect(() => {
 		const fetchRepositories = async () => {
@@ -54,50 +57,90 @@ export default function Home() {
 	const handleKeyDown = async (
 		event: React.KeyboardEvent<HTMLInputElement>,
 	) => {
-		if (event.key !== "Enter") {
-			return;
-		}
-
-		const url = event.currentTarget.value;
-		const match = url.match(githubRepositoryRegex);
-		if (!match) {
-			setError(
-				"Invalid URL format. Please enter a valid GitHub repository URL.",
-			);
-			return;
-		}
-
 		setError(null);
-		const username = match[2];
-		const repositoryName = match[3];
-
-		setLoading(true);
-		const response = await fetch(
-			`/api/usernames/${username}/repositories/${repositoryName}/commits`,
-			{
-				method: "GET",
-				headers: {
-					"Content-Type": "application/json",
-				},
-			},
-		);
-		setLoading(false);
-
-		if (response.status === 404) {
-			setError(
-				"Repository not found. Please enter a valid GitHub repository URL.",
+		if (event.key === "ArrowDown") {
+			setSelectedIndex((prevIndex) =>
+				Math.min(prevIndex + 1, suggestions.length - 1),
 			);
+			event.preventDefault();
 			return;
 		}
-		if (!response.ok) {
-			setError(
-				"Failed to search fisrt commit due to exceeding the GitHub API rate limit. Please try again after about 30 minutes.",
-			);
+		if (event.key === "ArrowUp") {
+			setSelectedIndex((prevIndex) => Math.max(prevIndex - 1, 0));
+			event.preventDefault();
+			return;
+		}
+		if (event.key === "Enter") {
+			if (selectedIndex >= 0) {
+				handleSuggestionClick(suggestions[selectedIndex]);
+			} else {
+				const url = event.currentTarget.value;
+				const match = url.match(githubRepositoryRegex);
+				if (!match) {
+					setError(
+						"Invalid URL format. Please enter a valid GitHub repository URL.",
+					);
+					return;
+				}
+
+				setError(null);
+				const username = match[2];
+				const repositoryName = match[3];
+
+				setLoading(true);
+				const response = await fetch(
+					`/api/usernames/${username}/repositories/${repositoryName}/commits`,
+					{
+						method: "GET",
+						headers: {
+							"Content-Type": "application/json",
+						},
+					},
+				);
+				setLoading(false);
+
+				if (response.status === 404) {
+					setError(
+						"Repository not found. Please enter a valid GitHub repository URL.",
+					);
+					return;
+				}
+				if (!response.ok) {
+					setError(
+						"Failed to search first commit due to exceeding the GitHub API rate limit. Please try again after about 30 minutes.",
+					);
+					return;
+				}
+
+				setError(null);
+				router.replace(`${username}/${repositoryName}`);
+			}
+		}
+	};
+
+	const handleInputChange = async (
+		event: React.ChangeEvent<HTMLInputElement>,
+	) => {
+		const query = event.target.value;
+		setInputValue(query);
+		if (query.length < 3) {
+			setSuggestions([]);
 			return;
 		}
 
-		setError(null);
-		router.replace(`${username}/${repositoryName}`);
+		const response = await fetch(`/api/repositories?q=${query}`);
+
+		const data: { repositories: Repository[] } = await response.json();
+		setSuggestions(data.repositories);
+	};
+
+	const handleSuggestionClick = (repo: Repository) => {
+		if (!repo) {
+			return;
+		}
+		setInputValue(`https://github.com/${repo.username}/${repo.name}`);
+		setSuggestions([]);
+		setSelectedIndex(-1);
 	};
 
 	if (loading) {
@@ -124,16 +167,33 @@ export default function Home() {
 					<input
 						type="url"
 						placeholder="https://github.com/Doarakko/git-first-commit"
+						value={inputValue}
 						onKeyDown={handleKeyDown}
+						onChange={handleInputChange}
 						onFocus={() => setError(null)}
 						className="appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-700 rounded-md focus:outline-hidden focus:ring-blue-500 focus:border-blue-500 focus:z-10 sm:text-sm bg-white"
 					/>
+
+					{suggestions.length > 0 && (
+						<ul className="absolute bg-white border border-gray-300 rounded-md mt-1 z-10 w-md">
+							{suggestions.map((repo, index) => (
+								<li
+									key={repo.id}
+									className={`p-2 cursor-pointer ${selectedIndex === index ? "bg-gray-100" : ""}`}
+									onClick={() => handleSuggestionClick(repo)}
+									onMouseEnter={() => setSelectedIndex(index)}
+								>
+									{repo.username}/{repo.name}
+								</li>
+							))}
+						</ul>
+					)}
 					{error && <div className="text-red-500 text-sm mt-2">{error}</div>}
 				</div>
 			</div>
 
 			<div>
-				<h2 className="text-xl font-bold pt-24 pb-2 pl-2 text-gray-700">
+				<h2 className="text-xl font-bold pt-36 pb-2 pl-2 text-gray-700">
 					Featured
 				</h2>
 				{repositories.length > 0 && (
